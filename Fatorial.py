c961aaabc5dbd20193651701472c6b9f022eed1d
Alesal2021 <alessandrocontato1975@gmail.com>
2022-02-10 09:45:41 -0300

    Initial commit

--- Fatorial.py
+++ Fatorial.py
@@ -1,7 +1,20 @@
+def valida_int(pergunta, mim, max):
+    x = int(input(pergunta))
+    while x < mim or x > max:
+        x = int(input(pergunta))
+    return x
 def fatorial(num):
+    """
+    Calcular o fatorial
+    :param num:
+    :return:
+    """
     fat = 1
     if num == 0:
         return fat
     for i in range(1, num +1, 1):
         fat *= i
     return fat
+x = int(input('Digite um valor para calcular o fatorial', 0, 99999))
+print('{}! = {} '.format(x, fatorial(x)))
+help(fatorial())
