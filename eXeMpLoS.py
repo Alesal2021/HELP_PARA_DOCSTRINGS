522513ac8beb23cfbdf90ca1529cfce5c5e5bf59
Alesal2021 <alessandrocontato1975@gmail.com>
2022-02-10 09:17:19 -0300

    Initial commit

---
+++ eXeMpLoS.py
@@ -0,0 +1,14 @@
+def soma(x = 0, y = 0, z = 0):
+    """
+    Clica 3 asterisco e depois ENTER
+    Função que soma ate 3 valores
+    :param x:Valor inteiros opcionais
+    :param y:Valor inteiros opcionais
+    :param z:Valor inteiros opcionais
+    :return:A soma dos inteiros x,y,z
+    """
+    res = x + y + z
+
+#Programa Principal
+print(soma(2,3))
+help(soma)
